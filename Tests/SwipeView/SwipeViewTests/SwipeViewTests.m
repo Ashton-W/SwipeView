4364c615d2106d90a6e29179f5fa234c296b9046
Ashton-W <Ashton-W@users.noreply.github.com>
2014-04-29 16:21:54 +1000

    Added test project

---
+++ Tests/SwipeView/SwipeViewTests/SwipeViewTests.m
@@ -0,0 +1,29 @@
+//
+//  SwipeViewTests.m
+//  SwipeViewTests
+//
+//  Created by Ashton Williams on 29/04/2014.
+//  Copyright (c) 2014 Charcoal Design. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SwipeViewTests : XCTestCase
+
+@end
+
+@implementation SwipeViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+@end
